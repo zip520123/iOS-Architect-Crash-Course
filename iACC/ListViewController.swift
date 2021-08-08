39f81609e3c09e6aa63783a44db1145833bd8615
zip520123 <zip520123@yahoo.com.tw>
2021-08-08 13:06:14 +0100

    Create itemViewModel in the right context to improve the design and eliminate fatal errors required by untyped models

--- iACC/ListViewController.swift
+++ iACC/ListViewController.swift
@@ -70,19 +70,43 @@ class ListViewController: UITableViewController {
 		if fromFriendsScreen {
 			FriendsAPI.shared.loadFriends { [weak self] result in
 				DispatchQueue.mainAsyncIfNeeded {
-					self?.handleAPIResult(result)
+                    self?.handleAPIResult(result.map({ (friends) in
+                        if User.shared?.isPremium == true {
+                            (UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate).cache.save(friends)
+                        }
+                        return friends.map { (friend) in
+                            ItemViewModel(friend: friend) {
+                                self?.select(friend: friend)
+                            }
+                        }
+                    }))
 				}
 			}
 		} else if fromCardsScreen {
 			CardAPI.shared.loadCards { [weak self] result in
 				DispatchQueue.mainAsyncIfNeeded {
-					self?.handleAPIResult(result)
+                    self?.handleAPIResult(result.map({ (cards) in
+                        cards.map { (card) in
+                            ItemViewModel(card: card) {
+                                self?.select(card: card)
+                            }
+                        }
+                    }))
 				}
 			}
 		} else if fromSentTransfersScreen || fromReceivedTransfersScreen {
-			TransfersAPI.shared.loadTransfers { [weak self] result in
+			TransfersAPI.shared.loadTransfers { [weak self, longDateStyle, fromSentTransfersScreen] result in
 				DispatchQueue.mainAsyncIfNeeded {
-					self?.handleAPIResult(result)
+                    
+                    self?.handleAPIResult(result.map({ (transfers) in
+                        transfers
+                            .filter { fromSentTransfersScreen ? $0.isSender : !$0.isSender }
+                            .map { (transfer) in
+                            ItemViewModel(transfer: transfer, longDateStyle: longDateStyle) {
+                                self?.select(transfer: transfer)
+                            }
+                        }
+                    }))
 				}
 			}
 		} else {
@@ -90,36 +114,12 @@ class ListViewController: UITableViewController {
 		}
 	}
 	
-	private func handleAPIResult<T>(_ result: Result<[T], Error>) {
+	private func handleAPIResult(_ result: Result<[ItemViewModel], Error>) {
 		switch result {
 		case let .success(items):
-			if fromFriendsScreen && User.shared?.isPremium == true {
-				(UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate).cache.save(items as! [Friend])
-			}
+			
 			self.retryCount = 0
-			
-			var filteredItems = items as [Any]
-			if let transfers = items as? [Transfer] {
-				if fromSentTransfersScreen {
-					filteredItems = transfers.filter(\.isSender)
-				} else {
-					filteredItems = transfers.filter { !$0.isSender }
-				}
-			}
-			
-            self.items = filteredItems.map({ (item) in
-                ItemViewModel(item, longDateStyle: longDateStyle, selection: {[weak self] in
-                    if let friend = item as? Friend {
-                        self?.select(friend: friend)
-                    } else if let card = item as? Card {
-                        self?.select(card: card)
-                    } else if let transfer = item as? Transfer {
-                        self?.select(transfer: transfer)
-                    } else {
-                        fatalError("unknown item: \(item)")
-                    }
-                })
-            })
+            self.items = items
 			self.refreshControl?.endRefreshing()
 			self.tableView.reloadData()
 			
